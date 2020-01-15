4b38e447b1a91ed5afd383a6d4ef77ac8307f7f1
RosenV95 <59887435+RosenV95@users.noreply.github.com>
2020-01-15 17:55:49 +0200

    NewCommit

---
+++ Spring MVC/Calculator/src/main/java/com/softuni/entity/Calculator.java
@@ -0,0 +1,57 @@
+package com.softuni.entity;
+
+public class Calculator {
+    private double leftOperand;
+    private double rightOperand;
+    private String operator;
+
+    public Calculator(double leftOperand, double rightOperand, String operator) {
+        this.leftOperand = leftOperand;
+        this.rightOperand = rightOperand;
+        this.operator = operator;
+    }
+    public Calculator(){
+
+    }
+
+    public double getLeftOperand() {
+        return leftOperand;
+    }
+
+    public double getRightOperand() {
+        return rightOperand;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+    public double calculateResult(){
+        double result=0;
+        switch(this.operator){
+            case"+":
+                result=this.getLeftOperand()+this.getRightOperand();
+                break;
+            case"-":
+                result=this.getLeftOperand()-this.getRightOperand();
+                break;
+            case"*":
+                result=this.getLeftOperand()*this.getRightOperand();
+                break;
+            case"/":
+                result=this.getLeftOperand()/this.getRightOperand();
+                break;
+            case "^":
+                result=Math.pow(this.getLeftOperand(),this.getRightOperand());
+                break;
+            case "√":
+             result=Math.sqrt(this.getLeftOperand());
+             break;
+            case "% of":
+                result=(getLeftOperand()/100)*getRightOperand();
+                break;
+
+        }
+        return result;
+    }
+
+}
